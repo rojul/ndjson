c21b03192dfba1a741af199803c9926d1be177dd
Julian <julian@jull.dev>
2021-11-14 01:25:30 +0100

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::{IntoApp, Parser};
 use serde_json::Value;
-use std::io::{self, BufRead, Write};
+use std::io::{self, BufRead};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 #[derive(Parser, Debug)]
@@ -36,57 +36,60 @@ fn main() -> io::Result<()> {
     let mut stdout = ColoredWriter::new(StandardStream::stdout(ColorChoice::Always));
 
     for line in stdin.lock().lines() {
-        let line = line?;
-        match serde_json::from_str(&line) {
-            Ok(Value::Object(object)) if !object.is_empty() => write_object(&mut stdout, &object),
-            Ok(value) if value.as_array().map_or(false, |array| !array.is_empty()) => {
-                write_value(&mut stdout, &value)
-            }
-            _ => stdout.write(&line),
-        }?;
-        stdout.set_kind(TokenKind::None).write("\n")?;
+        write_line(&mut stdout, &line?)?;
     }
 
     Ok(())
 }
 
-fn write_value(stdout: &mut ColoredWriter, value: &Value) -> io::Result<()> {
+fn write_line<T: WriteColor>(writer: &mut ColoredWriter<T>, line: &str) -> io::Result<()> {
+    match serde_json::from_str(line) {
+        Ok(Value::Object(object)) if !object.is_empty() => write_object(writer, &object),
+        Ok(value) if value.as_array().map_or(false, |array| !array.is_empty()) => {
+            write_value(writer, &value)
+        }
+        _ => writer.write(line),
+    }?;
+    writer.set_kind(TokenKind::None).write("\n")
+}
+
+fn write_value<T: WriteColor>(writer: &mut ColoredWriter<T>, value: &Value) -> io::Result<()> {
     match value {
-        Value::String(string) => stdout.set_kind(TokenKind::String).write(string),
+        Value::String(string) => writer.set_kind(TokenKind::String).write(string),
         Value::Array(array) => {
-            stdout.set_kind(TokenKind::None).write("[")?;
+            writer.set_kind(TokenKind::None).write("[")?;
             for (index, value) in array.iter().enumerate() {
                 if index != 0 {
-                    stdout.set_kind(TokenKind::None).write(", ")?;
+                    writer.set_kind(TokenKind::None).write(", ")?;
                 }
-                write_value(stdout, value)?;
+                write_value(writer, value)?;
             }
-            stdout.set_kind(TokenKind::None).write("]")
+            writer.set_kind(TokenKind::None).write("]")
         }
         Value::Object(object) => {
             if object.is_empty() {
-                stdout.set_kind(TokenKind::None).write("{}")
+                writer.set_kind(TokenKind::None).write("{}")
             } else {
-                stdout.set_kind(TokenKind::None).write("{ ")?;
-                write_object(stdout, object)?;
-                stdout.set_kind(TokenKind::None).write(" }")
+                writer.set_kind(TokenKind::None).write("{ ")?;
+                write_object(writer, object)?;
+                writer.set_kind(TokenKind::None).write(" }")
             }
         }
-        _ => stdout.set_kind(TokenKind::Value).write(&value.to_string()),
+        _ => writer.set_kind(TokenKind::Value).write(&value.to_string()),
     }
 }
 
-fn write_object(
-    stdout: &mut ColoredWriter,
+fn write_object<T: WriteColor>(
+    writer: &mut ColoredWriter<T>,
     object: &serde_json::Map<String, Value>,
 ) -> io::Result<()> {
     for (index, (key, value)) in object.iter().enumerate() {
         if index != 0 {
-            stdout.write(" ")?;
+            writer.write(" ")?;
         }
-        stdout.set_kind(TokenKind::Key).write(key)?;
-        stdout.set_kind(TokenKind::None).write(": ")?;
-        write_value(stdout, value)?;
+        writer.set_kind(TokenKind::Key).write(key)?;
+        writer.set_kind(TokenKind::None).write(": ")?;
+        write_value(writer, value)?;
     }
     Ok(())
 }
@@ -99,16 +102,16 @@ enum TokenKind {
     String,
 }
 
-struct ColoredWriter {
-    stream: StandardStream,
+struct ColoredWriter<T: WriteColor> {
+    writer: T,
     kind: TokenKind,
     deferred: bool,
 }
 
-impl ColoredWriter {
-    fn new(stream: StandardStream) -> Self {
+impl<T: WriteColor> ColoredWriter<T> {
+    pub fn new(writer: T) -> Self {
         ColoredWriter {
-            stream,
+            writer,
             kind: TokenKind::None,
             deferred: false,
         }
@@ -122,7 +125,7 @@ impl ColoredWriter {
         self
     }
 
-    fn write(&mut self, string: &str) -> io::Result<()> {
+    pub fn write(&mut self, string: &str) -> io::Result<()> {
         if string.is_empty() {
             return Ok(());
         }
@@ -134,13 +137,87 @@ impl ColoredWriter {
                 TokenKind::String => Some(Color::Cyan),
             };
             match color {
-                None => self.stream.reset(),
+                None => self.writer.reset(),
                 Some(color) => self
-                    .stream
+                    .writer
                     .set_color(ColorSpec::new().set_fg(Some(color)).set_intense(true)),
             }?;
             self.deferred = false
         }
-        self.stream.write_all(string.as_bytes())
+        self.writer.write_all(string.as_bytes())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use termcolor::Buffer;
+
+    fn format(buffer: Buffer, input: &str) -> String {
+        let mut buffer = ColoredWriter::new(buffer);
+        write_line(&mut buffer, input).unwrap();
+        let mut output = String::from_utf8(buffer.writer.into_inner()).unwrap();
+        assert_eq!(output.pop(), Some('\n'));
+        output
+    }
+
+    #[test]
+    fn test_color() {
+        assert_eq!(
+            format(
+                Buffer::ansi(),
+                r#"{"null":null,"string":"string","array":[1],"object":{"key":"value"}}"#
+            ),
+            "[0m[38;5;11mnull[0m: [0m[38;5;10mnull [0m[38;5;11mstring[0m: [0m[38;5;14mstring [0m[38;5;11marray[0m: [[0m[38;5;10m1[0m] [0m[38;5;11mobject[0m: { [0m[38;5;11mkey[0m: [0m[38;5;14mvalue[0m }"
+        );
+    }
+
+    #[test]
+    fn test_unchanged() {
+        for s in ["text", "0", "{   }", "[   ]"] {
+            assert_eq!(format(Buffer::ansi(), s), s);
+        }
+    }
+
+    #[test]
+    fn test_collections() {
+        for (input, output) in [
+            (r#"{"key":"value"}"#, "key: value"),
+            (r#"["value"]"#, "[value]"),
+            (r#"{"array":[],"object":{}}"#, "array: [] object: {}"),
+            (r#"[[],{}]"#, "[[], {}]"),
+            (
+                r#"{"array": ["value"],"object":{"key":"value"}}"#,
+                "array: [value] object: { key: value }",
+            ),
+            (
+                r#"[["value"],{"key":"value"}]"#,
+                "[[value], { key: value }]",
+            ),
+        ] {
+            assert_eq!(format(Buffer::no_color(), input), output);
+        }
+    }
+
+    #[test]
+    fn test_numbers() {
+        for (input, output) in [
+            ("0", "0"),
+            ("1234567890", "1234567890"),
+            ("0.01", "0.01"),
+            ("0.00", "0.0"),
+            ("1e2", "100.0"),
+        ] {
+            assert_eq!(
+                format(Buffer::no_color(), &format!("[{}]", input)),
+                format!("[{}]", output)
+            );
+        }
+    }
+
+    #[test]
+    fn test_empty_string() {
+        assert_eq!(format(Buffer::no_color(), r#"{"":""}"#), ": ");
+        assert_eq!(format(Buffer::no_color(), r#"[""]"#), "[]");
     }
 }
